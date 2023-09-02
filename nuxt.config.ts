40a11eb924c8814dc0b2f1f4b8f98ab37d323cde
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-09-02 10:59:28 +0300

    Проект налаштований та готовий то розробки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Allegria",
+      meta: [
+        { name: "description", content: "Allegria" },
+        { name: "keywords", content: "Allegria, Allegria, Allegria" },
+        // { name: "robots", content: "noindex, nofollow" },
+        // { name: "language", content: "English" },
+        { name: "author", content: "Dmitry Conquer" },
+      ],
+    },
+  },
+  devtools: { enabled: false },
+  spaLoadingTemplate: false,
+  css: ["~/assets/styles/style.scss"],
+  modules: [],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {
+        grid: true,
+        overrideBrowserslist: "last 2 versions",
+        },
+      ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    },
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+export default {
+   content: [
+     "./components/**/*.{js,vue,ts}",
+     "./layouts/**/*.vue",
+     "./pages/**/*.vue",
+     "./plugins/**/*.{js,ts}",
+     "./nuxt.config.{js,ts}",
+   ],
+   theme: {
+     container: {
+       center: true,
+       padding: "1rem",
+     },
+     extend: {
+       colors: {},
+       screens: {
+      },
+      fontSize: {
+      },
+      fontFamily: {
+      },
+     },
+   },
+ };
