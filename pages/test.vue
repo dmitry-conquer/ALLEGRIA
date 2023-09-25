28aa6b23bd443eeb2a9d360d41c40377feccd514
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-09-25 09:08:29 +0300

    updt

--- pages/test.vue
+++ pages/test.vue
@@ -7,32 +7,55 @@
         <ul>
           <li
             v-for="p in products"
-            :key="p.id">{{ p.name }}</li>
+            :key="p.id">
+            {{ p.name }}
+          </li>
         </ul>
       </div>
-      <hr>
+      <hr />
       <h2 class="text-center text-2xl font-bold uppercase text-indigo-500">1. useFetch in components</h2>
       <div>
         <ul>
           <li
             v-for="p in products2"
-            :key="p.id">{{ p.name }}</li>
+            :key="p.id">
+            {{ p.name }}
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <h2 class="text-center text-2xl font-bold uppercase text-indigo-500">1. useFetch in components</h2>
+      <div>
+        <ul>
+          <li
+            v-for="p in products3"
+            :key="p.id">
+            {{ p.name }}
+          </li>
         </ul>
       </div>
     </div>
   </div>
 </template>
 
-<script setup> 
+<script setup>
 const client = useSupabaseClient();
 
+const { data: products } = await client.from("products").select("*");
 
-const { data:products } = await client.from('products').select('*');
+const { data: products2 } = await useAsyncData("products2", async () => {
+  const { data } = await client.from("products").select("*");
+  return data;
+});
 
-const { data: products2 } = await useAsyncData('products2', async () => {
-  const { data } = await client.from('products').select('*');
-  return data
-})
-
-
+const { data: products3 } = await useAsyncData(
+  "products3",
+  async () => {
+    const { data } = await client.from("products").select("*");
+    return data;
+  },
+  {
+    lazy: true,
+  },
+);
 </script>
